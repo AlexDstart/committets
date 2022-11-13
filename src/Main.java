561c6e40095c785007a364c86a16314db2d716f0
AlexDstart <demyanalex@gmail.com>
2022-11-13 18:32:35 +0300

    коммит 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("коммит тест");
+        var friend = 15;
+        System.out.println(friend);
+
+    }
+}
